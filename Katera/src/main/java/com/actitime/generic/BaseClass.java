d53307568751a42c966b737b6ceef0f856e784aa
QSP <QSP@Dell-Laptop-2nd-Floor-room1>
2023-12-29 09:25:01 +0530

    Morning show

---
+++ Katera/src/main/java/com/actitime/generic/BaseClass.java
@@ -0,0 +1,8 @@
+package com.actitime.generic;
+
+public class BaseClass {
+
+public void openBrowser() {
+	
+}
+}
---
+++ Katera/src/test/java/com/actitime/testscript/UserModule.java
@@ -0,0 +1,10 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+public class UserModule {
+	@Test
+public void addUser() {
+	Reporter.log("addUser",true);
+}
+}
